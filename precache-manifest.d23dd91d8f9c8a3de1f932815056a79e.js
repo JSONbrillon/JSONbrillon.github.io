4ae003a09da6c77a16adb2523c775897db52f2ff
Jason Brillon <JSONbrillon@gmail.com>
2020-08-30 10:56:00 -0400

    Updates

---
+++ precache-manifest.d23dd91d8f9c8a3de1f932815056a79e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "384708202e621862e39c5e947839cf80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a2b9020c289b19eb886",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "72cf5d3328f165fef23e",
+    "url": "/static/js/2.22df621d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.22df621d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a2b9020c289b19eb886",
+    "url": "/static/js/main.a4cecc06.chunk.js"
+  },
+  {
+    "revision": "cf2c0bcf69de43034090",
+    "url": "/static/js/runtime-main.1c5ea697.js"
+  },
+  {
+    "revision": "62f14c4967a0355d8e0af82811ecc447",
+    "url": "/static/media/Resume.62f14c49.pdf"
+  },
+  {
+    "revision": "79c308b0aff81d22a581f13e40d18ae1",
+    "url": "/static/media/norway.79c308b0.jpg"
+  }
+]);
